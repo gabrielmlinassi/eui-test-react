cc8e532fcde6330bd1094596d000edae95f03084
Gabriel Marmitt <gabrielm.linassi@gmail.com>
2023-07-23 01:06:22 -0300

    remove test api call

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,3 @@
-import { useEffect } from "react";
-
 export const HomePage = () => {
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await fetch("/api");
-        const data = await response.json();
-        console.log({ data });
-      } catch (error) {
-        console.error({ error });
-      }
-    };
-
-    getData();
-  }, []);
-
   return <h1>Home Page...</h1>;
 };
